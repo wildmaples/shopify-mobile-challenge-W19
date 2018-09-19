b839d5a18919f6ae1b1338d11494e3d778696931
mpj <maplepeijune@gmail.com>
2018-09-18 21:10:27 -0400

    Initial Commit

---
+++ TagsAndProductList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TagsAndProductList
+//
+//  Created by maple peijune on 2018-09-18.
+//  Copyright © 2018 maple peijune. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
